98f207c0a6f6118650eaf5799aab15cdd07ee4f2
VuthyDara <149569078+DaraVuthyPCC@users.noreply.github.com>
2025-05-30 17:09:11 +1000

    set datatype for parameter

--- classes/inboxMessage.py
+++ classes/inboxMessage.py
@@ -2,7 +2,7 @@ import itertools
 
 class inboxMessage:
     _id_counter = itertools.count(start=0)
-    def __init__(self, message):
+    def __init__(self, message: str):
         self.inboxMessageID = next(inboxMessage._id_counter)
         self.message = message
         self.isRead = False       
