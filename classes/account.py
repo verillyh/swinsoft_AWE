7857fc1d52e6f52c08bf92c12804390f315849bc
VuthyDara <149569078+DaraVuthyPCC@users.noreply.github.com>
2025-05-30 17:20:58 +1000

    implement inboxInterface

--- classes/account.py
+++ classes/account.py
@@ -1,40 +1,60 @@
 import itertools
 from filter import Statisticable
+from abc import ABC, abstractmethod
 
-class Account:
-    _id_counter = itertools.count(start=-1)
-    def __init__(self, accountPrivilege, streetAddress: str, username, password):
+class InboxInterface(ABC):
+    @abstractmethod
+    def showInboxMessage():
+        pass
+
+class Account(InboxInterface):
+    _id_counter = itertools.count(start=0)
+    def __init__(self, accountPrivilege: int, email: str, streetAddress: str, username: str, passwordHash: str):
         self.accountID = next(Account._id_counter)
         self.accountPrivilege = accountPrivilege
+        self.email = email
         self.streetAddress = streetAddress
         self.username = username
-        self.password = password
+        self.passwordHash = passwordHash
         self.orderHistory = []
         self.inboxMessage = []
 
-    def validate():
+    @staticmethod
+    def hashPassword():
 
-        return False
+        return str
 
+    @staticmethod
     def login():
 
-        return False
+        return None
 
+    @staticmethod
     def signup():
 
-        return False
+        return None
     
+    @staticmethod
     def modifyAccountDetail():
 
-        return False
+        return bool
     
+    @staticmethod
     def verifyCredentials():
 
-        return False
+        return bool
+    
+    def loadDetails():
+        
+        return None
+    
+    def showInboxMessage():
+        
+        return None
     
 class ownerAccount(Account):
-    def __init__(self, accountPrivilege, streetAddress, username, _password):
-        super().__init__(accountPrivilege, streetAddress, username, _password)
+    def __init__(self, email, streetAddress, username, password):
+        super().__init__(1, email, streetAddress, username, password)
         self.generatedStatistics = []
 
     def createStatistics():
@@ -45,16 +65,23 @@ class ownerAccount(Account):
 
         return False
     
+    def addStaff():
+
+        return None
+    
+    def removeStaff():
+
+        return None
+
+class staffAccount(Account):
+    def __init__(self, email, streetAddress, username, password):
+        super().__init__(2, email, streetAddress, username, password)
+        self.receipt = []
+        self.invoice = []
+
 class customerAccount(Account):
-    def __init__(self, accountPrivilege, streetAddress, username, _password, cart, payment):
-        super().__init__(accountPrivilege, streetAddress, username, _password)
+    def __init__(self, email, streetAddress, username, password, cart):
+        super().__init__(3, email, streetAddress, username, password)
         self.receipt = []
         self.invoice = []
         self.cart = cart
-        self.payment = payment
-
-class staffAccount(Account):
-    def __init__(self, accountPrivilege, streetAddress, username, _password):
-        super().__init__(accountPrivilege, streetAddress, username, _password)
-        self.receipt = []
-        self.invoice = []
