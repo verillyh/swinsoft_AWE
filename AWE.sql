e734aa2fbe420db3d517fea14f7d37279a8d2f8d
VuthyDara <149569078+DaraVuthyPCC@users.noreply.github.com>
2025-06-05 10:49:52 +1000

    add sql script

---
+++ AWE.sql
@@ -0,0 +1,191 @@
+DROP TABLE IF EXISTS OrderItem;
+DROP TABLE IF EXISTS Invoice;
+DROP TABLE IF EXISTS OrderRecord;
+DROP TABLE IF EXISTS ProductGood;
+DROP TABLE IF EXISTS BrandCategory;
+DROP TABLE IF EXISTS Brand;
+DROP TABLE IF EXISTS Category;
+DROP TABLE IF EXISTS Account;
+
+CREATE TABLE Account (
+    AccountID       INT             AUTO_INCREMENT  PRIMARY KEY,
+    Email           VARCHAR(100)    NOT NULL        UNIQUE,
+    UserName        VARCHAR(100)    NOT NULL,
+    Password        VARCHAR(100)    NOT NULL,
+    StreetAddress   VARCHAR(100)    NOT NULL,
+    AccountType     ENUM('OWNER', 'STAFF', 'CUSTOMER') NOT NULL
+);
+
+CREATE TABLE OrderRecord (
+    OrderID         INT             AUTO_INCREMENT  PRIMARY KEY,
+    CustomerID      INT             NOT NULL,
+    OrderDate       DATETIME        NOT NULL,
+    OrderStatus     ENUM('PENDING', 'PAID', 'SHIPPED', 'CANCELLED') NOT NULL,
+    TotalPrice      DECIMAL(10,2)   NOT NULL,
+    CustomerName    VARCHAR(100)    NOT NULL,
+    PhoneNumber     VARCHAR(100)    NOT NULL,
+    ShippingAddress VARCHAR(100)    NOT NULL,
+    FOREIGN KEY (CustomerID) REFERENCES Account(AccountID)
+);
+
+CREATE TABLE Invoice (
+    InvoiceID       INT             AUTO_INCREMENT  PRIMARY KEY,
+    OrderID         INT             NOT NULL,
+    CustomerID      INT             NOT NULL,
+    AmountDue       DECIMAL(10,2)   NOT NULL,
+    InvoiceStatus   ENUM('PENDING', 'PAID', 'CANCELLED') NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES OrderRecord(OrderID),
+    FOREIGN KEY (CustomerID) REFERENCES Account(AccountID)
+);
+
+CREATE TABLE Category (
+    CategoryID      INT             AUTO_INCREMENT  PRIMARY KEY,
+    CategoryName    VARCHAR(100)    NOT NULL        UNIQUE
+);
+
+INSERT INTO Category (CategoryName)
+VALUES
+  ('TELEVISION'),
+  ('MOBILEPHONE'),
+  ('COMPUTER_LAPTOP');
+
+CREATE TABLE Brand (
+    BrandID         INT             AUTO_INCREMENT  PRIMARY KEY,
+    BrandName       VARCHAR(100)    NOT NULL        UNIQUE
+);
+
+INSERT INTO Brand (BrandName)
+VALUES
+  ('SAMSUNG'),
+  ('LG'),
+  ('SONY'),
+  ('PANASONIC'),
+  ('TCL'),
+  ('APPLE'),
+  ('GOOGLE'),
+  ('ONEPLUS'),
+  ('XIAOMI'),
+  ('MOTOROLA'),
+  ('DELL'),
+  ('HP'),
+  ('LENOVO'),
+  ('ASUS'),
+  ('ACER');
+
+CREATE TABLE BrandCategory (
+    BrandID         INT             NOT NULL,
+    CategoryID      INT             NOT NULL,
+    PRIMARY KEY (BrandID, CategoryID),
+    FOREIGN KEY (BrandID)    REFERENCES Brand(BrandID)    ON DELETE CASCADE,
+    FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID) ON DELETE CASCADE
+);
+
+INSERT INTO BrandCategory (BrandID, CategoryID)
+VALUES
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'SAMSUNG'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'TELEVISION')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'SAMSUNG'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'MOBILEPHONE')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'SAMSUNG'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'LG'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'TELEVISION')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'LG'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'SONY'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'TELEVISION')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'SONY'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'PANASONIC'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'TELEVISION')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'TCL'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'TELEVISION')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'APPLE'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'MOBILEPHONE')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'APPLE'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'GOOGLE'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'MOBILEPHONE')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'ONEPLUS'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'MOBILEPHONE')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'XIAOMI'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'MOBILEPHONE')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'MOTOROLA'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'MOBILEPHONE')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'DELL'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'HP'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'LENOVO'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'ASUS'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  ),
+  (
+    (SELECT BrandID FROM Brand WHERE BrandName = 'ACER'),
+    (SELECT CategoryID FROM Category WHERE CategoryName = 'COMPUTER_LAPTOP')
+  );
+
+
+CREATE TABLE ProductGood (
+    ProductID       INT             AUTO_INCREMENT  PRIMARY KEY,
+    Name            VARCHAR(100)    NOT NULL,
+    Description     VARCHAR(100)    NOT NULL,
+    UnitPrice       DECIMAL(10,2)   NOT NULL,
+    StockQuantity   INT             NOT NULL,
+    CategoryID      INT             NOT NULL,
+    BrandID         INT             NOT NULL,
+    FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID),
+    FOREIGN KEY (BrandID)    REFERENCES Brand(BrandID),
+    FOREIGN KEY (BrandID, CategoryID)
+        REFERENCES BrandCategory(BrandID, CategoryID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE OrderItem (
+    OrderItemID     INT             AUTO_INCREMENT  PRIMARY KEY,
+    OrderID         INT             NOT NULL,
+    ProductID       INT             NOT NULL,
+    Quantity        INT             NOT NULL,
+    UnitPrice       DECIMAL(10,2)   NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES OrderRecord(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES ProductGood(ProductID)
+);
