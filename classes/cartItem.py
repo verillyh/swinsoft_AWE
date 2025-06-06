70b7628e3ae9a88763cc225ef206c102447b68eb
VuthyDara <149569078+DaraVuthyPCC@users.noreply.github.com>
2025-06-06 10:03:23 +1000

    formatting cartitem

--- classes/cartItem.py
+++ classes/cartItem.py
@@ -1,8 +1,9 @@
 class CartItem:
-    def __init__(self, product: dict, quantity: int):
+    def __init__(self, product: dict, quantity: int, is_selected: bool = False):
         self._product = product
         self._quantity = quantity
         self.__cartItemID: int | None = None
+        self.is_selected = is_selected
 
     def set_cart_item_id(self, new_id: int):
         self.__cartItemID = new_id
